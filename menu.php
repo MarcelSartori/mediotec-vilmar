2d3ed5e633439536b154447b164bb7af03289354
MarcelSartori <contato@mslwebstudio.com.br>
2018-12-07 20:04:22 -0300

    (07/12) Adição do Menu Dropdown
    Adicionado o dropdown do menu com as categorias cadastradas listadas como opções do menu.

--- menu.php
+++ menu.php
@@ -1,3 +1,9 @@
+<?php
+// Crio minha consultao ao banco
+$sqlmenu = "SELECT * FROM categorias ORDER BY nome ASC";
+// Armazeno o resultado do comando na variável $resultado
+$resultado_menu = mysql_query($sqlmenu);
+?>
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -15,7 +21,16 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li><a href="index.php">Página Inicial</a></li>
-        <li><a href="noticias.php">Notícias</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Notícias <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <?php
+            while ($categoria = mysql_fetch_array($resultado_menu)) {
+              echo '<li><a href="link">'.$categoria['nome'].'</a></li>';
+            }
+            ?>
+          </ul>
+        </li>
         <li><a href="contato.php">Contato</a></li>
       </ul>
       
